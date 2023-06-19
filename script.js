015641cba3e42ff74a51287b7b5d5bc8d15b24e1
your-m1nd <vi.neurogirl@gmail.com>
2023-06-19 23:40:28 +0300

    add changes

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 let cities = ['Москва', 'Санкт-Петербург', 'Нью-Йорк', 'Токио'];
 let temperatures = [];
 
-//for (let city of cities) {
-   // alert(city);
-  //}//
+
 for (let city of cities) {
 let result = prompt(`Температура в ${city}`);
 temperatures.push(result);
@@ -13,35 +11,8 @@ temperatures.push(result);
 
 //НЕ РАБОТАЕТ//
 for (let i = 0; i < cities.lenght; i++) {
-   let listOfCities = document.querySelector('#listOfCities');
-	let li = document.createElement('li');
+    let ul = document.getElementById('listOfCities');
+    let li = document.createElement('li');
 	li.textContent = `${cities[i]+temperatures[i]}`;
-	listOfCities.appendChild(li);
+	ul.appendChild(li);
 }
-
-//for (i = 0; i < cities.lenght; i++) {
-   // temperatures[i] = result;
-    
-    //temperatures.push(`${result}`);
-  //  console.log(temperatures);
-//}
-
-
-//let result = prompt();
-//let array = [];
-//for (let i = 0; i < result; i++) {
-//temperatures.push(result);
-//}
-
-
-
-
-
-
-//const listOfCities = document.createElement('ul');
-
-//for (i = 0; i < cities.lenght, i++;) {
-//const list = document.createElement('li');
- // list.textContent = `${temperature[i]+city[i]}`;
-//  listOfCities.append(list);
-//}
